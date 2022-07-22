052ecfec956534dd72408d110f95809ed4e86fd9
Caleb Ooi <cooi0004@student.monash.edu>
2022-07-22 23:45:34 +1000

    added default error controller

---
+++ src/main/java/com/caleb/SchoolDepartmentServer/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package com.caleb.SchoolDepartmentServer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public ResponseEntity<String> pageNotFound(){
+        return new ResponseEntity("page not found",HttpStatus.NOT_FOUND);
+
+    }
+}
